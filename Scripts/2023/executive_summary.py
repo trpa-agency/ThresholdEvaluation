e7759c7e60e7602cc5bac2430c1826fab634e0c0
TRPA-Sarah <snewsome@trpa.gov>
2025-01-10 09:48:44 -0800

    AQ Summary not done but Ken wants to look

--- Scripts/2023/executive_summary.py
+++ Scripts/2023/executive_summary.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from utils import *
 # get modules
 import arcpy
@@ -147,11 +146,10 @@ def plot_secchi_depth(df):
         include_plotlyjs="directory",
         div_id="1.3.c_Secchi_Depth",
     )
-=======
+
 #Get Air Quality Data for Threshold
-from utils import *
 # get path to save the file
-out_chart = local_path.parents[1] / '2023/ExecutiveSummary/Chart'
+out_chart = local_path.parents[1] / '2023/ExecutiveSummary/AirQuality'
 # config, template, and font for the charts
 config = {"displayModeBar": False}
 template = 'plotly_white'
@@ -175,58 +173,54 @@ def get_airquality_data_sql():
         # create dataframe from sql query
         df = pd.read_sql("SELECT * FROM sde_tabular.SDE.ThresholdEvaluation_AirQuality", conn)
     return df
-#Define colors of Bars
-colors={'PM2.5 - HIGH 24 HR':'#f4a261',
-                    'PM10 - HIGH 24 HR':'#2a9d8f',
-                    'CO - HIGH 8 HR':'#e76f51',
-                    'O3 - HIGH 1 HR ':'#264653',
-                    '3-year mean (90th Percentile, Mm-1)':'#e9c46a'}
+
+# Define colors of Bars
+colors = {
+    'PM2.5 - HIGH 24 HR': '#f4a261',
+    'PM10 - HIGH 24 HR': '#2a9d8f',
+    'CO - HIGH 8 HR': '#e76f51',
+    'O3 - HIGH 1 HR ': '#264653',
+    '3-year mean (90th Percentile, Mm-1)': '#e9c46a'
+}
+
 # Create Dataframe with Air Quality Data and Exceedances
 def plot_AQ_daily_exceedances_per_year(df, draft=False):
-    #Process Data
-    df_exceedances = df.groupby(['Year', 'Indicator'])['Exceedances'].mean().reset_index()
-    #df_exceedances.rename(columns={'Exceedances': 'Avg. Days of Exceedances'}, inplace=True)
-    df_exceedances = df_exceedances.pivot(index='Year', columns='Indicator', values='Exceedances')
-    #Define Threshold Values?
-    #threshold_values = {'PM2.5 - HIGH 24 HR': 35, 'PM10 - HIGH 24 HR': 50,'CO - HIGH 8 HR': 6, 'O3 - HIGH 1 HR ': 0.08, '3-year mean (90th Percentile, Mm-1)': 34 }
-    #Create Chart
-    #Create Chart
-    fig = px.bar(df_exceedances, x=df_exceedances.index, y=df_exceedances.columns, title='Annual Air Quality Exceedances Summary by Pollutant', color_discrete_map=colors)
+    # Process Data
+    df_exceedances = df[df['Indicator'].isin(colors.keys())]
+    df_exceedances = df_exceedances.groupby(['Year', 'Indicator'])['Exceedances'].mean().reset_index()
+    df_exceedances = df_exceedances.pivot(index='Year', columns='Indicator', values='Exceedances').reset_index()
+    
+    # Calculate average daily exceedances
+    df_exceedances = df_exceedances.melt(id_vars=['Year'], var_name='Indicator', value_name='Average Daily Exceedances')
+    print(df_exceedances)
+    # Create Chart
+    fig = px.bar(df_exceedances, x='Year', y='Average Daily Exceedances', color='Indicator', title='Annual Air Quality Exceedances Summary by Pollutant', barmode='group', color_discrete_map=colors)
 
     # Ensure that all years are displayed, even if there is no data for some
-    fig.update_layout(yaxis_title='Days Exceeding Standard',
-                  xaxis=dict(type='category', categoryorder='array', categoryarray=['PM2.5 - HIGH 24 HR', 'PM10 - HIGH 24 HR', 'CO - HIGH 8 HR', 'O3 - HIGH 1 HR ', '3-year mean (90th Percentile, Mm-1)']),
-                  font_family=font,  # Example font, replace with your font variable
-                  template=template,  # Example template, replace with your template variable
-                  showlegend=True,
-                  legend_title_text=None,
-                  dragmode=False,
-                  legend=dict(
-                orientation="h",
-                entrywidth=180,
-                yanchor="bottom",
-                y=1.05,
-                xanchor="right",
-                x=0.95,
-            ),
-                  hovermode="x unified")
-
-     # export chart
-    if draft == True:
+    fig.update_layout(
+        yaxis_title='Days Exceeding Standard',
+        xaxis=dict(type='category'),
+        font_family=font,
+        template=template,
+        showlegend=True,
+        legend_title_text=None,
+        dragmode=False,
+        hovermode="x unified"
+    )
+    
+    # Export chart
+    if draft:
         fig.write_html(
             config=config,
-            file= out_chart / "Draft/AirQuality_Summary.html",
-            # include_plotlyjs="directory",
-            div_id="AirQuality_Summary",
+            file=out_chart / "AirQuality_Summary_draft.html",
+            div_id="AirQuality_Summary_draft",
             full_html=False
-        )   
-    elif draft == False: 
+        )
+    else:
         fig.write_html(
             config=config,
-            file= out_chart / "Final/AirQuality_Summary.html",
-            # include_plotlyjs="directory",
+            file=out_chart / "AirQuality_Summary.html",
             div_id="AirQuality_Summary",
             full_html=False
         )
 
->>>>>>> 9e8e73d33db67abe3daf8a4d91246ca1ac82d1cb
